74f7c8950869ef4c35bf9a09a0661821682f65fd
Pedro Markun <pedro@markun.com.br>
2013-08-11 12:43:46 -0300

    adicionado paginador

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 //var my = new ES('http://127.0.0.1:9200', '');
+var q = {}
+
 var timeline = function(pl, data) {
 	  $.each(data, function(index, tramite) {
 	  	var tempo = tramite['tempo'];
@@ -8,12 +10,23 @@ var timeline = function(pl, data) {
 	  });
 	}
 
+var paginacao = function(direcao, q) {
+	if (direcao == 'mais') { 
+		q['from'] = q['from'] + q['size'];
+	}
+	else if (direcao == 'menos') {
+		if (q['from']-q['size'] >= 0) {
+			q['from'] = q['from'] - q['size'];
+		}
+	}
+	return q;
+}
+
 var render = function(q) {
 	var query = SETTINGS['SERVER'] + 'monitor/_search?source=' + JSON.stringify(q)
 	$.getJSON(query, function (data) {
 	    r.clear();
 	    $.each(data.hits.hits, function (index, t) {
-	    	console.log(t);
 	        r.append(t['_source']);
 	        timeline(t['_source']['id'], t['_source']['tramite']);
 	    });
@@ -23,25 +36,27 @@ var render = function(q) {
 
 $(document).ready(function () {
 	r = Tempo.prepare("tempo");
-	var q = {
+	q = {
       "query" : {
                 "match_all" : {}
             },
-        "size":10
+        "size":10,
+        "from":0
     }
 	render(q);
 
 	$("#buscar button").click(function (e) {
 		e.preventDefault();
 		var search = $("#buscar input").val();
-		var q = {
+		q = {
 			"query" : {
 				"query_string" : { 
 					"query" : search,
 					"fields" : ["ementa", "encerramento", "_id"],
 					"default_operator" : "AND"
 				},
-			"size":10
+			"size":10,
+			"from":0
 			}
 		}
 		render(q);
