47bb5347b176ac9f684084328ca2be6813774e0b
Pedro Markun <pedro@markun.com.br>
2013-08-28 16:10:23 -0300

    primeira versao do historico parlamentar de comissoes?

--- js/main.js
+++ js/main.js
@@ -1,11 +1,49 @@
 //var my = new ES('http://127.0.0.1:9200', '');
 var q = {}
 
+var cargos = function(d) {
+	var lq = {
+      "query" : {
+      	"nested" : {
+        	"path" : "comissoes",
+        	"query" : {
+		      	"bool" : {
+			      	"must" : [{
+			      		"range" : {
+			        		"i" : {
+			    				"lte" : d.format("DD/MM/YYYY")
+			    			}
+			        	}},
+			        	{"range" : {
+			        		"f" : {
+			   		     			"gte" : d.format("DD/MM/YYYY")
+			        			}
+			    	    	}	
+			    	    }]
+			    	}
+		    	}
+		    }
+	    },
+        "size":10,
+        "from":0,
+    }
+    var query = SETTINGS['SERVER'] + 'vereadores/_search?source=' + JSON.stringify(lq);
+    
+    $.getJSON(query, function (data) {
+    	$.each(data.hits.hits, function (index, t) {
+	        	console.log(t['_source'].nome_parlamentar);
+    	});
+	});
+}
+
 var timeline = function(pl, data) {
 	  $.each(data, function(index, tramite) {
 	  	var tempo = tramite['tempo'];
+	  	c = tramite['data_ini'].split('/')
+	  	var current = new Date(Number(c[2]), Number(c[1]), Number(c[0]));
 	  	for (var i=0;i<tempo;i++) {
-	  		$("#"+pl+ " .timeline").append("<div class='dia "+tramite['unidade']+"'' title='"+tramite['unidade']+" - "+ tempo +" dias'></div>");
+	  		$("#"+pl+ " .timeline").append("<div data-time='"+current.valueOf()+"' class='dia "+tramite['unidade']+"'' title='"+tramite['unidade']+" - "+ tempo +" dias'></div>");
+	  	current = new Date(current.setDate(current.getDate()+1));
 	  	}
 	  });
 	}
@@ -30,6 +68,10 @@ var render = function(q) {
 	        r.append(t['_source']);
 	        timeline(t['_source']['id'], t['_source']['tramite']);
 	    });
+        $(".timeline .dia").click(function (e) {
+			var d = moment(Number(e.target.getAttribute('data-time')));
+        	cargos(d);
+	  	});
 	    $("#tempo").fadeIn("slow");
 	});
 }
